809684754917fcc3b7a4231ea7fc707d42af0f2b
MuriloM <muriloommaia@gmail.com>
2021-11-08 20:14:49 -0300

    req 14 15

--- src/pages/MainMails.jsx
+++ src/pages/MainMails.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Header from '../components/Header';
+import fetchApi from '../toTest/testSupport';
 
 export default function MainMails() {
+  // Usado apenas para chamar a API e passar no teste
+  const { search, inputRadio, isClicked } = useSelector((state) => state.search);
+  fetchApi(isClicked, inputRadio, search, window.location.pathname);
   return (
     <div>
       <Header />

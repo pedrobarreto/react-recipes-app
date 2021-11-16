746805e45caac255deb62e7096267041668f8b91
MuriloM <muriloommaia@gmail.com>
2021-11-16 19:09:30 -0300

    requisito 53

--- src/components/favoritesAndDone/BodyFavoriteAndDone.jsx
+++ src/components/favoritesAndDone/BodyFavoriteAndDone.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import whiteHeartIcon from '../../images/whiteHeartIcon.svg';
 import blackHeartIcon from '../../images/blackHeartIcon.svg';
 import shareIcon from '../../images/shareIcon.svg';
@@ -6,11 +6,22 @@ import shareIcon from '../../images/shareIcon.svg';
 export default function BodyFavoriteAndDone() {
   const [favorite] = React.useState(true);
   const [clipboard, setClipboard] = React.useState(false);
-  const favorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
+  const [favoriteCards, setFavoriteCards] = React.useState([]);
+  useEffect(() => {
+    const favorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    setFavoriteCards(favorites);
+  }, []);
+  const handleClickFavorite = (itemObj) => {
+    const allFavorites = favoriteCards.filter(
+      (item) => item.id !== itemObj.id,
+    );
+    localStorage.setItem('favoriteRecipes', JSON.stringify(allFavorites));
+    setFavoriteCards(allFavorites);
+  };
   return (
     <div>
       <div>
-        { favorites.map((item, index) => {
+        { favoriteCards.map((item, index) => {
           const foodOrDrink = item.type === 'comida' ? 'area' : 'alcoholicOrNot';
           return (
             <div
@@ -48,7 +59,7 @@ export default function BodyFavoriteAndDone() {
                   type="button"
                   className="bg-transparent border-0"
                   data-testid={ `${index}-horizontal-favorite-btn` }
-                  // onClick={ handleClickFavorite }
+                  onClick={ () => handleClickFavorite(item) }
                   src={ favorite ? 'blackHeartIcon' : 'whiteHeartIcon' }
 
                 >
